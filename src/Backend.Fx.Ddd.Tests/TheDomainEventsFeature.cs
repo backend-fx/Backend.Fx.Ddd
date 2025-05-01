90ca5403df36900e20136ad4c91070392133b1f4
Marc Wittke <marc@wittke-web.de>
2025-05-01 13:47:45 -0300

    fix calling of domain event handlers when a single class combines handling of multiple events

--- src/Backend.Fx.Ddd.Tests/TheDomainEventsFeature.cs
+++ src/Backend.Fx.Ddd.Tests/TheDomainEventsFeature.cs
@@ -31,7 +31,7 @@ public class TheDomainEventsFeature : IAsyncLifetime
     [Fact]
     public async Task RaisedEventIsHandledOnCompleteOfOperation()
     {
-        var domainEvent = new TestEvent();
+        var domainEvent = new TestEvent1();
 
         await _app.Invoker.InvokeAsync((sp, _) =>
         {
@@ -52,17 +52,17 @@ public class TheDomainEventsFeature : IAsyncLifetime
         A.CallTo(() => UnusedTestEventHandler.Fake.HandleAsync(A<UnusedTestEvent>._, A<CancellationToken>._))
             .MustNotHaveHappened();
     }
-    
+
     [Fact]
     public async Task RaisedEventIsHandledOnCompleteOfOperationUsingOutboxPattern()
     {
-        var domainEvent = new TestEvent();
+        var domainEvent = new TestEvent1();
 
         await _app.Invoker.InvokeAsync((sp, _) =>
         {
             var entity = new EntityWithDomainEvents();
             entity.DomainEvents.Add(domainEvent);
-            
+
             sp.GetRequiredService<IDomainEventPublisher>().PublishDomainEvents(entity);
 
             // handling is postponed to the completion of the operation
@@ -81,21 +81,70 @@ public class TheDomainEventsFeature : IAsyncLifetime
             .MustNotHaveHappened();
     }
 
+    [Fact]
+    public async Task SingleHandlerCanHandleMultipleEvents()
+    {
+        var domainEvent2 = new TestEvent2();
+        var domainEvent3 = new TestEvent3();
+
+        await _app.Invoker.InvokeAsync((sp, _) =>
+        {
+            sp.GetRequiredService<IDomainEventPublisher>().PublishDomainEvent(domainEvent2);
+            sp.GetRequiredService<IDomainEventPublisher>().PublishDomainEvent(domainEvent3);
+
+            // handling is postponed to the completion of the operation
+            A.CallTo(() => MultiTestEventHandler.Fake2.HandleAsync(domainEvent2, A<CancellationToken>._))
+                .MustNotHaveHappened();
+            A.CallTo(() => MultiTestEventHandler.Fake3.HandleAsync(domainEvent3, A<CancellationToken>._))
+                .MustNotHaveHappened();
+
+            return Task.CompletedTask;
+        });
+
+        // now it must be handled
+        A.CallTo(() => MultiTestEventHandler.Fake2.HandleAsync(domainEvent2, A<CancellationToken>._))
+            .MustHaveHappenedOnceExactly();
+        A.CallTo(() => MultiTestEventHandler.Fake3.HandleAsync(domainEvent3, A<CancellationToken>._))
+            .MustHaveHappenedOnceExactly();
+    }
+
+
     public class EntityWithDomainEvents : IHaveDomainEvents
     {
         public DomainEventOutBox DomainEvents { get; } = new();
     }
-    
-    public class TestEvent : IDomainEvent;
+
+    public class TestEvent1 : IDomainEvent;
+
+    public class TestEvent2 : IDomainEvent;
+
+    public class TestEvent3 : IDomainEvent;
 
     [UsedImplicitly]
-    public class TestEventHandler : IDomainEventHandler<TestEvent>
+    public class TestEventHandler : IDomainEventHandler<TestEvent1>
     {
-        public static readonly IDomainEventHandler<TestEvent> Fake = A.Fake<IDomainEventHandler<TestEvent>>();
+        public static readonly IDomainEventHandler<TestEvent1> Fake = A.Fake<IDomainEventHandler<TestEvent1>>();
 
-        public async Task HandleAsync(TestEvent domainEvent, CancellationToken cancellationToken = default)
+        public async Task HandleAsync(TestEvent1 domainEvent1, CancellationToken cancellationToken = default)
         {
-            await Fake.HandleAsync(domainEvent, cancellationToken);
+            await Fake.HandleAsync(domainEvent1, cancellationToken);
+        }
+    }
+
+    [UsedImplicitly]
+    public class MultiTestEventHandler : IDomainEventHandler<TestEvent2>, IDomainEventHandler<TestEvent3>
+    {
+        public static readonly IDomainEventHandler<TestEvent2> Fake2 = A.Fake<IDomainEventHandler<TestEvent2>>();
+        public static readonly IDomainEventHandler<TestEvent3> Fake3 = A.Fake<IDomainEventHandler<TestEvent3>>();
+
+        public async Task HandleAsync(TestEvent2 domainEvent, CancellationToken cancellationToken = default)
+        {
+            await Fake2.HandleAsync(domainEvent, cancellationToken);
+        }
+
+        public async Task HandleAsync(TestEvent3 domainEvent, CancellationToken cancellationToken = default)
+        {
+            await Fake3.HandleAsync(domainEvent, cancellationToken);
         }
     }
 
@@ -105,7 +154,8 @@ public class TheDomainEventsFeature : IAsyncLifetime
     [UsedImplicitly]
     public class UnusedTestEventHandler : IDomainEventHandler<UnusedTestEvent>
     {
-        public static readonly IDomainEventHandler<UnusedTestEvent> Fake = A.Fake<IDomainEventHandler<UnusedTestEvent>>();
+        public static readonly IDomainEventHandler<UnusedTestEvent> Fake =
+            A.Fake<IDomainEventHandler<UnusedTestEvent>>();
 
         public async Task HandleAsync(UnusedTestEvent domainEvent, CancellationToken cancellationToken = default)
         {
